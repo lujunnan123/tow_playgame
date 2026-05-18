0ab6a22fd4a9d9d108f056501727f9b5d685d26e
Ljn123 <1106209907@qq.com>
2026-05-18 11:47:31 +0800

    0518-初始版

---
+++ src/stores/counter.js
@@ -0,0 +1,37 @@
+// 1. 引入定义仓库的方法
+import { defineStore } from 'pinia'
+// 2. 定义仓库（第一个参数是唯一 ID）
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    count: [1,2,3,4,5,6,7,8,9],  // 数据
+    weaponPackage:[
+        
+        {wpName:"离火扇",wpPrice:198,url:"../assets/logo.png"},
+        {wpName:"2021全球冠军赛",wpPrice:298,url:"../assets/images/exmp.png"},
+        {wpName:"2023全球冠军赛",wpPrice:298,url:"../assets/images/exmp.png"},
+        {wpName:"2024全球冠军赛",wpPrice:298,url:"../assets/images/exmp.png"},
+        {wpName:"VTC爪刀",wpPrice:298,url:"../assets/images/exmp.png"},
+        {wpName:"VTC2026篆刀",wpPrice:298,url:"../assets/images/exmp.png"},
+        {wpName:"VTC怜悯之刃",wpPrice:298,url:"../assets/images/exmp.png"},
+        {wpName:"怜悯2026",wpPrice:298,url:"../assets/images/exmp.png"},
+
+    ]
+  }),
+
+  // 计算属性
+  getters: {
+    doubleCount(state) {
+      return state.count * 2
+    },
+  },
+
+  // 方法（修改 state）
+  actions: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    },
+  },
+})
